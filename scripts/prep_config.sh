c5dc7e5392d2149a961fad6053b09367a55d3a12
Thomas Pasquier <thomas.francis.pasquier@gmail.com>
2020-06-01 18:15:27 +0000

    actually need to configure camflow to be loaded in the kernel

--- scripts/prep_config.sh
+++ scripts/prep_config.sh
@@ -4,4 +4,5 @@ for i in build/kernel/*-fedora.config
 do
 	echo $i
   cat ./extra-conf >> $i
+	sed -i -e "s/CONFIG_LSM=\"yama,loadpin,safesetid,integrity,selinux,smack,tomoyo,apparmor\"/CONFIG_LSM=\"yama,loadpin,safesetid,integrity,selinux,smack,tomoyo,apparmor,provenance\"/g" $i
 done
