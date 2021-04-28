7a4217db838a2758fc4fe454b00efca30fe707ec
Thomas Pasquier <thomas.francis.pasquier@gmail.com>
2021-04-28 10:14:17 +0100

    fix so the patches are properly applied

--- scripts/add_patch.sh
+++ scripts/add_patch.sh
@@ -4,6 +4,10 @@ LPATCH_ID=$(grep ^Patch ~/build/kernel/kernel.spec | tail -n1 | awk '{ print $1 
 NPATCH_ID=$(($LPATCH_ID + 1 ))
 sed -i "/^Patch$LPATCH_ID:\ /a#\ $DESC\nPatch$NPATCH_ID:\ 0001-information-flow.patch" ~/build/kernel/kernel.spec
 cat ~/build/kernel/kernel.spec | grep $NPATCH_ID
+LPATCH_ID=$(($LPATCH_ID + 1 ))
 NPATCH_ID=$(($NPATCH_ID + 1 ))
 sed -i "/^Patch$LPATCH_ID:\ /a#\ $DESC\nPatch$NPATCH_ID:\ 0002-camflow.patch" ~/build/kernel/kernel.spec
 cat ~/build/kernel/kernel.spec | grep $NPATCH_ID
+
+sed -i "/ApplyOptionalPatch patch-%{stableversion}-redhat.patch/ a ApplyOptionalPatch 0002-camflow.patch" ~/build/kernel/kernel.spec
+sed -i "/ApplyOptionalPatch patch-%{stableversion}-redhat.patch/ a ApplyOptionalPatch 0001-information-flow.patch" ~/build/kernel/kernel.spec
