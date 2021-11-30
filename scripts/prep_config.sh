bedd6034a9dc7a60518d333a0213c12335887ebf
Thomas Pasquier <thomas.francis.pasquier@gmail.com>
2021-11-29 22:49:16 -0800

    fix config update

--- scripts/prep_config.sh
+++ scripts/prep_config.sh
@@ -4,5 +4,5 @@ for i in ~/build/kernel/*-fedora.config
 do
 	echo $i
   cat ./extra-conf >> $i
-	sed -i -e "s/CONFIG_LSM=\"yama,loadpin,safesetid,integrity,selinux,smack,tomoyo,apparmor\"/CONFIG_LSM=\"yama,loadpin,safesetid,integrity,selinux,smack,tomoyo,apparmor,provenance\"/g" $i
+	sed -i -e "s/CONFIG_LSM=\"lockdown,yama,integrity,selinux,bpf,landlock\"/CONFIG_LSM=\"lockdown,yama,integrity,selinux,bpf,landlock,provenance\"/g" $i
 done
