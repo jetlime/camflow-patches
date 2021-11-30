f526be52a5323d6494a420cabaeb6656c511c6ef
Thomas Pasquier <thomas.francis.pasquier@gmail.com>
2021-11-30 01:54:09 -0800

    fix issue with patches not being applied

--- scripts/add_patch.sh
+++ scripts/add_patch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-LPATCH_ID=$(grep ^Patch ~/build/kernel/kernel.spec | tail -n1 | awk '{ print $1 }' | sed s/Patch// | sed s/://)
+LPATCH_ID=$(grep -m1 ^Patch ~/build/kernel/kernel.spec | awk '{ print $1 }' | sed s/Patch// | sed s/://)
 NPATCH_ID=$(($LPATCH_ID + 1 ))
 sed -i "/^Patch$LPATCH_ID:\ /a#\ $DESC\nPatch$NPATCH_ID:\ 0001-information-flow.patch" ~/build/kernel/kernel.spec
 cat ~/build/kernel/kernel.spec | grep $NPATCH_ID
@@ -9,5 +9,5 @@ NPATCH_ID=$(($NPATCH_ID + 1 ))
 sed -i "/^Patch$LPATCH_ID:\ /a#\ $DESC\nPatch$NPATCH_ID:\ 0002-camflow.patch" ~/build/kernel/kernel.spec
 cat ~/build/kernel/kernel.spec | grep $NPATCH_ID
 
-sed -i "/ApplyOptionalPatch patch-%{stableversion}-redhat.patch/ a ApplyOptionalPatch 0002-camflow.patch" ~/build/kernel/kernel.spec
-sed -i "/ApplyOptionalPatch patch-%{stableversion}-redhat.patch/ a ApplyOptionalPatch 0001-information-flow.patch" ~/build/kernel/kernel.spec
+sed -i "/ApplyOptionalPatch patch-%{patchversion}-redhat.patch/ a ApplyOptionalPatch 0002-camflow.patch" ~/build/kernel/kernel.spec
+sed -i "/ApplyOptionalPatch patch-%{patchversion}-redhat.patch/ a ApplyOptionalPatch 0001-information-flow.patch" ~/build/kernel/kernel.spec
